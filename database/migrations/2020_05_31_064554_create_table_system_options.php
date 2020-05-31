fa24c7c012e77dfccf8a8ca47d647dd6317c9b32
Mufarrihin Arham <farri.arham@gmail.com>
2020-05-31 13:49:50 +0700

    Initial

---
+++ database/migrations/2020_05_31_064554_create_table_system_options.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableSystemOptions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_system_options', function (Blueprint $table) {
+            $table->id();
+            $table->string('prefix_psb', 4);
+            $table->tinyInteger('tahun_aktif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_system_options');
+    }
+}
